a359704f0b0da77c3e3d2593646357818b806df8
ximenabaquero <saraximenagilbaquero@gmail.com>
2026-05-15 20:56:58 -0500

    Centrar cards Silencio Clínico con max-w-5xl mx-auto

--- src/features/home/components/ProblemSection.tsx
+++ src/features/home/components/ProblemSection.tsx
@@ -74,7 +74,7 @@ const ProblemSection = () => {
         </div>
 
         {/* Cards */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-16">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-16 max-w-5xl mx-auto">
           {problems.map((prob, index) => (
             <div
               key={index}
